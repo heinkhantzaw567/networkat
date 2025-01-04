ff2bbea0988eb920ca43773d4ee0780b27cada04
heinkhantzaw567 <135092601+heinkhantzaw567@users.noreply.github.com>
2025-01-04 01:24:59 -0500

    textarea

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Posts(models.Model):
+    userid = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
+
